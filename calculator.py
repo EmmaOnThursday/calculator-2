88fe0d2566eb698e791626f101a388a117095044
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 15:59:24 -0800

    user now able to quit by typing 'q'

--- calculator.py
+++ calculator.py
@@ -13,38 +13,40 @@ def calculator():
         input = raw_input(">") #get input from user here
         tokens = input.split(" ")
         #print len(tokens)
-        try:
-            num1 = int(tokens[1])
-            if len(tokens) == 3:
-                try:
-                    num2 = int(tokens[2])
-                    if tokens[0] == "+":
-                        return add(num1, num2)
-                    elif tokens[0] == "-":
-                        return subtract(num1, num2)
-                    elif tokens[0] == "*":
-                        return multiply(num1, num2)
-                    elif tokens[0] == "/":
-                        return divide(num1, num2)
-                    elif tokens[0] == "pow":
-                        return power(num1, num2)
-                    elif tokens[0] == "mod":
-                        return mod(num1, num2)
-                except (ValueError, NameError):
-                    return "Please enter problem blah blah balh" #next thing we were gonna do was specific form message for user
-            elif len(tokens) == 2:
-                if tokens[0] == "square":
-                    return square(num1)
-                elif tokens[0] == "cube":
-                    return cube(num1)
-            elif tokens[0] == "q":
-                break
-            else:
-                return "some error message here"
-        except (ValueError, NameError):
-            return "Please enter problem in the form [operand] plus [integer(s)]."
-        
+        if tokens[0] == "q":
+            break
+        else:
+            try:
+                num1 = int(tokens[1])
+                if len(tokens) == 3:
+                    try:
+                        num2 = int(tokens[2])
+                        if tokens[0] == "+":
+                            answer = add(num1, num2)
+                        elif tokens[0] == "-":
+                            answer = subtract(num1, num2)
+                        elif tokens[0] == "*":
+                            answer = multiply(num1, num2)
+                        elif tokens[0] == "/":
+                            answer = divide(num1, num2)
+                        elif tokens[0] == "pow":
+                            answer =  power(num1, num2)
+                        elif tokens[0] == "mod":
+                            answer =  mod(num1, num2)
+                    except (ValueError, NameError):
+                        answer =  "Please enter problem in the form [operand] plus [integer(s)]."
+                elif len(tokens) == 2:
+                    if tokens[0] == "square":
+                        answer =  square(num1)
+                    elif tokens[0] == "cube":
+                        answer =  cube(num1)
+                else:
+                    answer = "Please enter a problem in the form of [operand] plus [integer(s)]."
+            except (ValueError, NameError):
+                answer =  "Please enter problem in the form [operand] plus [integer(s)]."
+        print answer
+
 
         
         
-print calculator()
+calculator()
