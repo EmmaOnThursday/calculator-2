dc5e7367fb160687b15a6421ed36d7b549b17dfe
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 18:00:04 -0800

    work in progress: handling any number of inputs. DOES NOT WORK.

--- calculator.py
+++ calculator.py
@@ -12,38 +12,35 @@ def calculator():
     while True:
         input = raw_input(">") #get input from user here
         tokens = input.split(" ")
-        #print len(tokens)
+        print tokens
         if tokens[0] == "q":
             break
         else:
             try:
-                num1 = int(tokens[1])
-                if len(tokens) == 3:
-                    try:
-                        num2 = int(tokens[2])
-                        if tokens[0] == "+":
-                            answer = add(num1, num2)
-                        elif tokens[0] == "-":
-                            answer = subtract(num1, num2)
-                        elif tokens[0] == "*":
-                            answer = multiply(num1, num2)
-                        elif tokens[0] == "/":
-                            answer = divide(num1, num2)
-                        elif tokens[0] == "pow":
-                            answer =  power(num1, num2)
-                        elif tokens[0] == "mod":
-                            answer =  mod(num1, num2)
-                    except (ValueError, NameError):
-                        answer =  "Please enter problem in the form [operand] plus [integer(s)]."
-                elif len(tokens) == 2:
-                    if tokens[0] == "square":
-                        answer =  square(num1)
-                    elif tokens[0] == "cube":
-                        answer =  cube(num1)
-                else:
-                    answer = "Please enter a problem in the form of [operand] plus [integer(s)]."
+                arguments = []
+                for num in tokens[1:]:
+                    arguments.append(float(num))
+                num1 = float(tokens[1])
+                if tokens[0] == "+":
+                    answer = add(arguments)
+                elif tokens[0] == "-":
+                    answer = subtract(arguments)
+                elif tokens[0] == "*":
+                    answer = multiply(arguments)
+                elif tokens[0] == "/":
+                    answer = divide(arguments)
+                elif tokens[0] == "pow":
+                    answer =  power(num1, num2)
+                elif tokens[0] == "mod":
+                    answer =  mod(num1, num2)
+                elif tokens[0] == "square":
+                    answer =  square(num1)
+                elif tokens[0] == "cube":
+                    answer =  cube(num1)
+                else: 
+                    answer = "something something"
             except (ValueError, NameError):
-                answer =  "Please enter problem in the form [operand] plus [integer(s)]."
+                    answer =  "Error: non-integers entered."
         print answer
 
 
